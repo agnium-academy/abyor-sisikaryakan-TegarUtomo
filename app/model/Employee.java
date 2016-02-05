5971833297d3a2ba7bde11933e2b9dd611da38fc
TegarUtomo <mochamad.tegar.utomo@jaya-tech.com>
2016-02-05 15:52:43 +0700

    Create abstract class Employee and class Manager

---
+++ app/model/Employee.java
@@ -0,0 +1,38 @@
+package model;
+
+/**
+ * Created by User TEGAR on 2/5/2016.
+ */
+public abstract class Employee {
+
+    Integer id;
+    String nama;
+    String alamat;
+
+    public Employee() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
---
+++ app/model/manager/Manager.java
@@ -0,0 +1,9 @@
+package model.manager;
+
+import model.Employee;
+
+/**
+ * Created by User TEGAR on 2/5/2016.
+ */
+public class Manager extends Employee {
+}
